52d06239f9187991a42bc151baaeea7f6f6651e0
alicika <52126032+alicika@users.noreply.github.com>
2021-08-15 23:48:44 +0900

    update self-defined error

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,22 @@
-#[derive(Debug)]
+use std::io;
+use std::fmt;
+use std::error::Error;
+
+#[derive(Error)]
 pub enum AppError {
     MissingQuery,
     MissingFilename,
-    ConfigLoad,
+    ConfigLoad {
+        error: io::Error,
+    }
+}
+
+impl fmt::Display for AppError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::MissingQuery => f.write_str("No query string"),
+            Self::MissingFilename => f.write_str("No file name"),
+            Self::ConfigLoad { error } => write!(f, "Error state: {}", source),
+        }
+    }
 }
